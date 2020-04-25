c4a98804254f9e96dfaea98d3bd74a29f9ab2037
UlugBek <beka.eco.95@gmail.com>
2020-04-26 00:14:16 +0800

    [FIX] Social links

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -6,16 +6,16 @@ import { faFacebookF, faGithub, faInstagram, faLinkedinIn  } from '@fortawesome/
 export default function Socials() {
     return (
         <SocialsStyled>
-            <a href="" target="_blank" className="facebook">
+            <a href="https://www.facebook.com/ulugbek.abdullaev1" target="_blank" className="facebook">
                 <Icon icon={faFacebookF} />
             </a>    
-            <a href="" target="_blank" className="github">
+            <a href="https://github.com/ulugbek-dev" target="_blank" className="github">
                 <Icon icon={faGithub} />
             </a>  
-            <a href="" target="_blank" className="instagram">
+            <a href="https://www.instagram.com/ulugbek.08/" target="_blank" className="instagram">
                 <Icon icon={faInstagram} />
             </a>
-            <a href="" target="_blank" className="linkedin">
+            <a href="https://www.linkedin.com/in/ulugbek-abduloev-347169ba/" target="_blank" className="linkedin">
                 <Icon icon={faLinkedinIn} />
             </a>
         </SocialsStyled>
